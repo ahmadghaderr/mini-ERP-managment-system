a589b84db631aff911b9496c34c7d803fcb685bf
Rafca Youssef <rafcas.youssef@gmail.com>
2026-08-11 17:17:28 +0300

    add invoice and stock page

---
+++ frontend/src/components/invoices/list.tsx
@@ -0,0 +1,11 @@
+// src/components/invoices/list.tsx
+import './list.css';
+
+export default function InvoicesList() {
+  return (
+    <div className="invoices-list">
+      <h1>Invoices</h1>
+      {/* TODO: table of uploaded invoices with status (uploaded / extracting / pending review / approved) */}
+    </div>
+  );
+}
